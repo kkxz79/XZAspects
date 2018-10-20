76838b103b8d6c975d5e085db1735aea0e11c911
kkxz79 <kkxz79@126.com>
2018-10-20 16:12:42 +0800

    first commit

---
+++ XZAspects/XZMenuView.h
@@ -0,0 +1,17 @@
+//
+//  XZMenuView.h
+//  XZAspects
+//
+//  Created by kkxz on 2018/10/20.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XZMenuView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XZAspects/XZMenuView.m
@@ -0,0 +1,21 @@
+//
+//  XZMenuView.m
+//  XZAspects
+//
+//  Created by kkxz on 2018/10/20.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import "XZMenuView.h"
+
+@implementation XZMenuView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
